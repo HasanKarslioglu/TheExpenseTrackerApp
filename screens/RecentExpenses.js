8c23653c244f27580134906c530f9c972aaa2cd5
HasanKarslioglu <86919390+HasanKarslioglu@users.noreply.github.com>
2023-07-24 20:56:56 +0300

    131

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,7 +1,7 @@
-import { Text } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
 function RecentExpenses() {
-  return <Text>RecentExpenses</Text>;
+  return <ExpensesOutput expensesPeriod="Last 7 Days" />;
 }
 
 export default RecentExpenses;
